28526b7a3c0eb22cd7304f7e130b4d8627b4de5c
Angela Mumba <malaika.mumba@gmail.com>
2019-05-30 19:18:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end
  def self.new_by_filename(filename)
     parts = filename.split(" - ")
-    new_song = self.new(song)
+    new_song = Song.new(song)
     new_song.artist_name = artist
     new_song.save
     
