abd36a64e8afe89743de7a4405c161559838d1ce
Angela Mumba <malaika.mumba@gmail.com>
2019-05-30 19:21:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,10 +25,10 @@ class Song
     new_song
   end
 
-  # def artist_name=(name)
-  #   self.artist = Artist.find_or_create_by_name(name)
-  #   artist.add_song(self)
-  # end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 end
 # require 'pry'
 # class Song
